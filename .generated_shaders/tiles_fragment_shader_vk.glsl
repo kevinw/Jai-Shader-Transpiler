b9421b8eb8a5b2a58683b47d6a826bbe738b0f24
Kevin Watters <kevinwatters@gmail.com>
2026-02-08 23:59:28 +0100

    Generated shaders.

---
+++ .generated_shaders/tiles_fragment_shader_vk.glsl
@@ -0,0 +1,33 @@
+#version 450 core
+
+layout(set = 0, binding = 0) uniform FragmentShaderVk_Uniforms_Block {
+    vec2 u_resolution;
+    float u_time;
+} un;
+
+layout (location=0) out vec4 out_color;
+
+
+void main() {
+     vec2 u_resolution = un.u_resolution;
+     float u_time = un.u_time;
+     float aspect_ratio = u_resolution.y / u_resolution.x;
+     vec2 uv = vec2(gl_FragCoord.x, gl_FragCoord.y) / u_resolution.x;
+     uv -= vec2(0.5, 0.5 * aspect_ratio);
+     float rot = radians(-30 - u_time);
+     mat2 rotation_matrix = mat2(cos(rot), -sin(rot), sin(rot), cos(rot));
+     uv = rotation_matrix * uv;
+     vec2 scaled_uv = 20 * uv;
+     vec2 tile = fract(scaled_uv);
+     float tile_dist = min(min(tile.x, 1 - tile.x), min(tile.y, 1 - tile.y));
+     float square_dist = length(floor(scaled_uv));
+     float edge = sin(u_time - square_dist * 20);
+     edge = mod(edge * edge, edge / edge);
+     float value = mix(tile_dist, 1 - tile_dist, step(1, edge));
+     edge = pow(abs(1 - edge), 2.2) * 0.5;
+     value = smoothstep(edge - 0.05, edge, 0.95 * value);
+     value += square_dist * 0.1;
+     value *= 0.8 - 0.2;
+     out_color = vec4(pow(value, 2), pow(value, 1.5), pow(value, 1.2), 1);
+}
+
