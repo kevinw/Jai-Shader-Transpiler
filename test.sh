588a48baacb48fb40d1b268598ddd15961e4d4a8
Kevin Watters <kevinwatters@gmail.com>
2026-02-08 18:43:03 +0100

    move generated shaders into .generated_shaders

--- test.sh
+++ test.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 set -euo pipefail
+export MTL_LOG_TO_STDERR=1
 export MTL_DEBUG_LAYER=1
+export MTL_LOG_LEVEL=MTLLogLevelDebug
+export MTL_LOG_BUFFER_SIZE=2048
 jai -quiet test_runner.jai +Autorun
