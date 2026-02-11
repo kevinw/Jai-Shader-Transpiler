8f390413e417c9f6960506e0e7df4a109e97c61f
Kevin Watters <kevinwatters@gmail.com>
2026-02-11 18:00:51 +0100

    pass spirv backend down

--- .generated_shaders/bindless_nested_vertex_vk.glsl
+++ .generated_shaders/bindless_nested_vertex_vk.glsl
@@ -1,29 +1,26 @@
 #version 450
-layout(row_major) uniform;
-layout(row_major) buffer;
-struct BindlessNestedVk_VertexData_0
+
+struct BindlessNestedVk_VertexData_std430_logical
 {
-    vec2 offset_0;
+    vec2 offset;
 };
 
-layout(std430, binding = 0) readonly buffer StructuredBuffer_BindlessNestedVk_VertexData_t_0 {
-    BindlessNestedVk_VertexData_0 _data[];
-} params_vertex_data_0;
-layout(location = 0)
-in vec2 input_a_pos_0;
-
-struct bindless_nested_vertex_vk_Out_0
+struct BindlessNestedVk_VertexData_std430
 {
-    vec4 Ugl_Position_0;
+    vec2 offset;
 };
+
+layout(set = 0, binding = 0, std430) readonly buffer StructuredBuffer
+{
+    BindlessNestedVk_VertexData_std430 _m0[];
+} params_vertex_data;
+
+layout(location = 0) in vec2 input_a_pos;
 
 void main()
 {
-    bindless_nested_vertex_vk_Out_0 o_0;
-    o_0.Ugl_Position_0 = vec4(0.0);
-    vec2 p_0 = params_vertex_data_0._data[uint(0)].offset_0 + input_a_pos_0;
-    o_0.Ugl_Position_0 = vec4(p_0.x, p_0.y, 0.0, 1.0);
-    gl_Position = o_0.Ugl_Position_0;
-    return;
+    BindlessNestedVk_VertexData_std430_logical _31;
+    _31.offset = params_vertex_data._m0[0].offset;
+    gl_Position = vec4(_31.offset + input_a_pos, 0.0, 1.0);
 }
 
--- .generated_shaders/compute_write_indices_vk.glsl
+++ .generated_shaders/compute_write_indices_vk.glsl
@@ -1,17 +1,16 @@
 #version 450
-layout(row_major) uniform;
-layout(row_major) buffer;
-layout(std430, binding = 0) buffer StructuredBuffer_uint_t_0 {
-    uint _data[];
-} values_0;
 layout(local_size_x = 1, local_size_y = 1, local_size_z = 1) in;
+
+layout(set = 0, binding = 0, std430) buffer values
+{
+    uint _m0[];
+} values_1;
+
 void main()
 {
-    uint _S1 = gl_GlobalInvocationID.x;
-    if(_S1 < 64U)
+    if (gl_GlobalInvocationID.x < 64u)
     {
-        values_0._data[uint(_S1)] = _S1;
+        values_1._m0[gl_GlobalInvocationID.x] = gl_GlobalInvocationID.x;
     }
-    return;
 }
 
