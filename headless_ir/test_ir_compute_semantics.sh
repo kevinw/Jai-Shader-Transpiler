3d50512fefeb6595280e36ee6a243be719222a8d
Kevin Watters <kevinwatters@gmail.com>
2026-02-16 20:49:20 +0100

    cleanup / test speed

--- headless_ir/test_ir_compute_semantics.sh
+++ headless_ir/test_ir_compute_semantics.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 set -euo pipefail
-jai -quiet headless_ir/build_ir_compute_semantics.jai - "$@"
+SCRIPT_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd)"
+jai -quiet "${SCRIPT_DIR}/build_ir_compute_semantics.jai" - "$@"
